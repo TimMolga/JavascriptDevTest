84d3d8b58fd56e0adc9a59b692e5a6d22a6dd77e
timot <timothymolga@gmail.com>
2020-02-23 22:57:47 -0500

    further refactored code, added css

--- scripts/index.js
+++ scripts/index.js
@@ -22,10 +22,9 @@ function AddPair() {
         const formattedInput = pairValue.trim();
         nameValuePairList.push(formattedInput);
         PopulateNameValuePairList(formattedInput);
-        error.textContent = ''
+        error.textContent = '';
     } else {
-        error.textContent =
-            'Invalid key value pair.'
+        error.textContent = 'Invalid key value pair.';
     }
     pair.value = '';
 }
@@ -61,9 +60,7 @@ function SortValue() {
 function DeletePair() {
     const pair = document.getElementById('nameValuePairList');
     //delete selected options from listbox and array
-    for (
-        let selectedIndex = pair.options.length - 1; selectedIndex >= 0; selectedIndex--
-    ) {
+    for (let selectedIndex = pair.options.length - 1; selectedIndex >= 0; selectedIndex--) {
         if (pair.options[selectedIndex].selected) {
             pair.remove(selectedIndex);
             nameValuePairList.splice(selectedIndex, 1);
@@ -75,18 +72,25 @@ function DeletePair() {
 function ShowXML() {
     const xmlContent = document.getElementById('xmlArea');
     xmlContent.textContent = '';
+    const spc = '\u00A0';
+    const nl = '\r\n';
     //split the array pairs and put them into text on the page
     const pairs = nameValuePairList.map(pair => pair.split('='));
     const splitPairs = pairs.map(
         pair =>
-        '\u00A0<KeyValue>\r\n\u00A0\u00A0<Key>' +
-        pair[0] +
-        '</Key>\r\n' +
-        '\u00A0\u00A0<Value>' +
-        pair[1] +
-        '</Value>\r\n\u00A0</KeyValue>\r\n'
+        spc + spc + '<KeyValue>' +
+        nl + spc + spc + spc +
+        '<Key>' + pair[0] + '</Key>' +
+        nl + spc + spc + spc +
+        '<Value>' + pair[1] + '</Value>' +
+        nl + spc + spc +
+        '</KeyValue>' + nl
     );
-    const html = '<KeyValues>\r\n' + splitPairs.join('') + '</KeyValues>';
+    const html =
+        'XML Content' + nl + nl +
+        '<KeyValues>' + nl +
+        splitPairs.join('') +
+        '</KeyValues>';
     xmlContent.textContent = html;
 }
 
